259c0f6edd37b08c152e458127939d380970bd47
SH <shardacker@gmail.com>
2018-08-31 12:19:40 +0200

    Refactoring; Input eingebaut

--- EZA/Program.cs
+++ EZA/Program.cs
@@ -8,45 +8,45 @@ namespace EZA
 {
     class Program
     {
-        const int cellCount = 120;
-        const int genCount = 75;
+        const int genCountDefault = 75;
+        const int cellCountDefault = 150;
+        const string printSign = "X";
 
         static void Main(string[] args)
         {
-            for (int r = 0; r < 256; r++)
+            while (true)
             {
-                //Get current rule (0..255)
-                string[] rule = GetRule(r);
-                Console.WriteLine($"Rule #: {r} {string.Join("",rule)}");
+                int cellCount = GetCellCount();
+                int genCount = GetGenCount();
+                int ruleNr = GetRuleNr();
 
-                //Initialize Generation 0
-                string[] gen = new string[cellCount];
-                gen.Populate("0");
-                gen[(cellCount / 2)] = "1";
-                string[] newGen;
-                PrintGeneration(0, gen);
+                PrintPattern(cellCount, genCount, ruleNr);
 
-                //Jede einzelne Generation berechnen und ausgeben
-                for (int i = 1; i <= genCount; i++)
-                {
-                    newGen = new string[cellCount];
-                    for (int c = 0; c < cellCount; c++)
-                    {
-                        string pre1 = c > 0 ? gen[c - 1] : "0";
-                        string pre2 = gen[c];
-                        string pre3 = c < cellCount - 1 ? gen[c + 1] : "0";
-                        int ruleIndex = rule.Length - Convert.ToInt32(pre1 + pre2 + pre3, 2) - 1;
-                        newGen[c] = rule[ruleIndex];
-                    }
-                    gen = newGen;
-                    PrintGeneration(i, gen);
-                }
-                Console.ReadKey();
+                Console.WriteLine("Press any to continue...");
+                Console.ReadKey(true);
                 Console.Clear();
             }
         }
 
-        private static string[] GetRule(int ruleNr)
+        private static void PrintPattern(int cellCount, int genCount, int ruleNr)
+        {
+            string rule = GetBinaryRule(ruleNr);
+            Console.WriteLine($"{cellCount} cells; {genCount} generations; Rule #{ruleNr} ({rule})");
+
+            //Initialize Generation 0
+            string[] gen = new string[cellCount];
+            gen.Populate("0");
+            gen[cellCount / 2] = "1";
+            PrintGeneration(gen);
+
+            for (int i = 1; i <= genCount; i++)
+            {
+                gen = GenerateGeneration(gen, rule);
+                PrintGeneration(gen);
+            }
+        }
+
+        private static string GetBinaryRule(int ruleNr)
         {
             string ruleBin = Convert.ToString(ruleNr, 2);
             string[] rule = new string[8] { "0", "0", "0", "0", "0", "0", "0", "0", };
@@ -56,13 +56,26 @@ namespace EZA
             {
                 rule[offset + i] = ruleBin[i].ToString();
             }
-            return rule;
+            return string.Join("", rule);
         }
 
-        private static void PrintGeneration(int genCount, string[] gen)
+        private static string[] GenerateGeneration(string[] gen, string rule)
         {
-            //Console.Write("G{0:0#}: ", genCount);
+            int cells = gen.Length;
+            string[] newGen = new string[cells];
+            for (int c = 0; c < cells; c++)
+            {
+                string pre1 = c > 0 ? gen[c - 1] : "0";
+                string pre2 = gen[c];
+                string pre3 = c < cells - 1 ? gen[c + 1] : "0";
+                int ruleIndex = 8 - Convert.ToInt32(pre1 + pre2 + pre3, 2) - 1;
+                newGen[c] = rule[ruleIndex].ToString();
+            }
+            return newGen;
+        }
 
+        private static void PrintGeneration(string[] gen)
+        {
             foreach (string cell in gen)
             {
                 if (cell == "0")
@@ -71,11 +84,114 @@ namespace EZA
                 }
                 else if (cell == "1")
                 {
-                    Console.Write("X");
+                    Console.Write(printSign);
                 }
             }
             Console.WriteLine();
         }
+
+        #region Input
+        private static int GetCellCount()
+        {
+            int cellCount;
+            while (true)
+            {
+                Console.Write($"Cells (default {cellCountDefault}): ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    cellCount = cellCountDefault;
+                }
+                else
+                {
+                    try
+                    {
+                        cellCount = int.Parse(input);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid input");
+                        continue;
+                    }
+                }
+                if (cellCount < 8)
+                {
+                    Console.WriteLine("Min. 8 cells.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return cellCount;
+        }
+
+        private static int GetGenCount()
+        {
+            int genCount;
+            while (true)
+            {
+                Console.Write($"Generations (default {genCountDefault}): ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    genCount = genCountDefault;
+                }
+                else
+                {
+                    try
+                    {
+                        genCount = int.Parse(input);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid input");
+                        continue;
+                    }
+                }
+                if (genCount < 1)
+                {
+                    Console.WriteLine("Min. 1 generation.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return genCount;
+        }
+
+        private static int GetRuleNr()
+        {
+            int genCount;
+            while (true)
+            {
+                Console.Write("Rule: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    genCount = int.Parse(input);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+                if (genCount > 255)
+                {
+                    Console.WriteLine("Max. rule is 255.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return genCount;
+        } 
+        #endregion
     }
 
     static class ArrayExtension
